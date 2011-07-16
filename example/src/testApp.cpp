1abebcf17a65496993a1a9170c4df1e4bd1a11c9
ben <ben@nerdlabor.de>
2011-07-16 20:07:38 +0200

    changed example for win testing

--- example/src/testApp.cpp
+++ example/src/testApp.cpp
@@ -8,39 +8,26 @@ void testApp::setup() {
 	ofSetFrameRate(60);
 
 	ofBackground(0,0,0);
-	mousePos[ofxFensterManager::get()->getPrimaryWindow()->id]=ofVec2f(0,0);
-	
-	int winW=300;
-	for(int i=0;i<3;i++){
-		ofxFenster* win=ofxFensterManager::get()->createFenster(400+(i*winW), 0, winW, 300, OF_WINDOW);
-		win->addListener(this);
-		mousePos[win->id]=ofVec2f(0,0);
-	}
-
-	//setup of fensterListener does not get called automatically yet
-	imgWin.setup();
-	for(int i=0;i<3;i++){
-		ofxFenster* win=ofxFensterManager::get()->createFenster(400+(i*winW), 300, winW, 300, OF_WINDOW);
-		if(i==0){
-			ofAddListener(win->events.mouseMoved, this, &testApp::mouseMovedEvent);
-		}
-		win->addListener(&imgWin);
-		win->setWindowTitle("image render "+ofToString(i+1));
-	}
 }
 
 //--------------------------------------------------------------
 void testApp::update() {
-	
+        if(ofGetFrameNum() == 10)
+        {
+            imgWin.setup();
+            ofxFenster* win=ofxFensterManager::get()->createFenster(400, 300, 300, 300, OF_WINDOW);
+            ofAddListener(win->events.mouseMoved, this, &testApp::mouseMovedEvent);
+            win->addListener(&imgWin);
+        }
 }
 
 //--------------------------------------------------------------
 void testApp::draw() {
 	ofVec2f mp=mousePos[ofxFensterManager::get()->getActiveWindow()->id];
 	ofVec2f p;
-	
+
 	float dSquared=100*100;
-	
+
 	for(int x=0; x<ofGetWidth(); x+=40) {
 		for(int y=0; y<ofGetHeight(); y+=40) {
 			p.set(x, y);
@@ -53,7 +40,7 @@ void testApp::draw() {
 
 //--------------------------------------------------------------
 void testApp::keyPressed(int key, ofxFenster* win) {
-	
+
 }
 
 //--------------------------------------------------------------
@@ -69,37 +56,37 @@ void testApp::mouseMoved(int x, int y, ofxFenster* win)
 
 //--------------------------------------------------------------
 void testApp::mouseMoved(int x, int y ) {
-	
+
 }
 
 //--------------------------------------------------------------
 void testApp::mouseDragged(int x, int y, int button) {
-	
+
 }
 
 //--------------------------------------------------------------
 void testApp::mousePressed(int x, int y, int button) {
-	
+
 }
 
 //--------------------------------------------------------------
 void testApp::mouseReleased(int x, int y, int button) {
-	
+
 }
 
 //--------------------------------------------------------------
 void testApp::windowResized(int w, int h) {
-	
+
 }
 
 //--------------------------------------------------------------
 void testApp::gotMessage(ofMessage msg) {
-	
+
 }
 
 //--------------------------------------------------------------
 void testApp::dragEvent(ofDragInfo dragInfo) {
-	
+
 }
 
 void testApp::mouseMovedEvent(ofMouseEventArgs &args){
